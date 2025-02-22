0d8ef807190dfee3145de216c2c1528c0e3214cb
Yumihoshi <xiaoyuesun915@gmail.com>
2025-02-22 20:33:54 +0800

    [add] 单例基类
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

---
+++ Scripts/TestSingleton.cs
@@ -0,0 +1,17 @@
+﻿// *****************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/02/22 20:02
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using Godot;
+using LumiVerseFramework.Base;
+
+namespace LumiVerseFramework1.Scripts;
+
+public partial class TestSingleton : Singleton<TestSingleton>
+{
+    
+}
