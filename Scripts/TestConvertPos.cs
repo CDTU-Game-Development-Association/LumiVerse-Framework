e1d100f3e13323acc35278e6508e5c998c5472c1
Yumihoshi <xiaoyuesun915@gmail.com>
2025-02-25 19:19:06 +0800

    [add] 屏幕坐标和世界坐标互转
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

---
+++ Scripts/TestConvertPos.cs
@@ -0,0 +1,34 @@
+﻿// *****************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/02/25 19:02
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using Godot;
+using LumiVerseFramework.Common;
+
+namespace LumiVerseFramework1.Scripts;
+
+public partial class TestConvertPos : Node
+{
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (@event is InputEventMouseButton mouseEvent &&
+            mouseEvent.Pressed &&
+            mouseEvent.ButtonIndex == MouseButton.Left)
+        {
+            // 获取屏幕坐标
+            Vector2 screenPos = mouseEvent.Position;
+
+            // 转换为世界坐标
+            Vector2 worldPos =
+                YumihoshiConvert.ConvertScreenToWorld(screenPos, this);
+
+            YumihoshiDebug.Print<TestConvertPos>("测试屏幕坐标转换为世界坐标",
+                $"点击位置 - 屏幕: {screenPos} 世界: {worldPos}");
+        }
+    }
+}
