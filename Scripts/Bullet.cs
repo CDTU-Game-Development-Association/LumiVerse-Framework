c961a1117d7745e46febd90e4f4de7b479427621
Yumihoshi <xiaoyuesun915@gmail.com>
2025-03-06 18:27:33 +0800

    [add] 对象池
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿// *****************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/03/06 18:03
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using Godot;
+using LumiVerseFramework.Base.Interfaces;
+
+namespace LumiVerseFramework1.Scripts;
+
+public partial class Bullet : Node2D, IObjPool
+{
+}
